ad5cfb9433457f64b39fb135e62ceb4de2ed58c7
Shingo miura <miugonis@icloud.com>
2017-08-26 13:26:31 +0000

    favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,12 @@ class UsersController < ApplicationController
     counts(@user)
   end
   
+  def favorites
+    @user = User.find(params[:id])
+    @microposts = @user.favorite_microposts.order('created_at DESC').page(params[:page])
+    counts(@user)
+    render :show
+  end
   
   private
   
